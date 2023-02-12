4403cf04a88f4d2c145ae01610c5f4ff324683a8
efodthe2nd <davidefod@gmail.com>
2023-02-12 10:09:17 +0000

    0

---
+++ models/base_model.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Module base_model
+
+This Module contains a definition for BaseModel Class
+"""
+
+from datetime import datetime
+import uuid
+from models import storage
+
+
+import models
+
+
+class BaseModel:
+    """BaseModel Class"""
+
+    def __init__(self, *args, **kwargs):
+        """__init__ method & instantiation of class Basemodel
+
+        Args:
+            *args.
+            **kwargs (dict): Key/value pairs
+        """
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
+        if kwargs is not None and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            models.storage.new(self)
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+        bs_dict = ({
+            k:v.isoformat() if isinstance(v, datetime) else v
+            for (k, v) in self.__dict__.items()
+            })
+        bs_dict["__class__"] = self.__class__.__name__
+        return bs_dict
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def __str__(self):
+        """should print/str representation of the BaseModel instance."""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
+
